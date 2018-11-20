71289b64fa4cfd14188fd102764034d94b2543a9
DESKTOP-5TH3GMF\faghi <faghirnavaz@gmail.com>
2018-11-19 21:53:49 -0500

    Initilize

---
+++ Northwind/NorthWind.Domain/Entities/EmployeeTerritory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWind.Domain.Entities
+{
+    public class EmployeeTerritory
+    {
+        public int EmployeeId { get; set; }
+        public string TerritoryId { get; set; }
+
+        public Employee Employee { get; set; }
+        public Territory Territory { get; set; }
+    }
+}
---
+++ Northwind/NorthWind.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using NorthWind.Domain.ValueObjects;
+
+namespace NorthWind.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public AdAccount AdAccount { get; set; }
+    }
+}
