ac2f7a032e8e32cc8d43258b06ec2b92ad065777
DESKTOP-5TH3GMF\faghi <faghirnavaz@gmail.com>
2018-11-24 23:30:54 -0500

    persiatance layer

---
+++ Northwind/NorthWind.Common/IDateTime.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NorthWind.Common
+{
+    public interface IDateTime
+    {
+        DateTime Now { get; }
+    }
+}
---
+++ Northwind/Northwind.Infrastructure/MachineDateTime.cs
@@ -0,0 +1,12 @@
+﻿using NorthWind.Common;
+using System;
+
+namespace Northwind.Infrastructure
+{
+    public class MachineDateTime : IDateTime
+    {
+        public DateTime Now => DateTime.Now;
+        public int CurrentYear => DateTime.Now.Year;
+
+    }
+}
